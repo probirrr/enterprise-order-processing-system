73094af9a4b083b290b177e48add6090a4347a72
probirrr <probirdey0987@gmail.com>
2026-01-16 20:16:57 +0530

    Add get order by id API

--- spring-boot-api/src/main/java/com/enterprise/orderapi/controller/OrderController.java
+++ spring-boot-api/src/main/java/com/enterprise/orderapi/controller/OrderController.java
@@ -1,13 +1,16 @@
 package com.enterprise.orderapi.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class OrderController {
 
-    @GetMapping("/orders")
-    public String getOrders() {
-        return "Order list fetched successfully";
-    }
+    @GetMapping("/orders/{id}")
+   public String getOrderById(@PathVariable int id) {
+    return "Order details for order id: " + id;
 }
+}
+
